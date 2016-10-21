f4b681ea673104f7e60e3a993f85fa693df08d3b
Nabil <nabiljesus@gmail.com>
2016-10-21 18:48:12 -0400

    done with ida_gap

--- global/ida_gap.cpp
+++ global/ida_gap.cpp
@@ -16,6 +16,7 @@ void signalHandler( int signum )
    exit(signum);  
 
 }
+unsigned nodos_gen = 0;
 
 tuple<unsigned,unsigned,bool> bounded_id_dfs(state_t state, int  d, int  bound, int history);
 
@@ -27,7 +28,6 @@ int main(int argc, char **argv) {
 	char first_state[511];
 	state_t state;
     bool goal;
-    unsigned nodos_gen;
 
     signal(SIGALRM,signalHandler);
 
@@ -46,12 +46,12 @@ int main(int argc, char **argv) {
     cout << "X, ida_gap, pancake28, " << "\""<< first_state << "\", " << flush ;
 
     bound=gap_h(&state,NUMVARS);
-    nodos_gen = 0;
+    //int costo = 0;
     //Perform depth-bounded searches with increasing depth bounds
     while (true){
         nodos_gen++;
-    	tuple<unsigned,unsigned,bool> p = bounded_id_dfs(state, bound, bound, init_history);
-        nodos_gen+= get<1>(p);
+    	tuple<unsigned,unsigned,bool> p = bounded_id_dfs(state, 0, bound, init_history);
+        //costo+= get<1>(p);
         bound =  get<1>(p);
     	if (get<2>(p) == 1){
             t_fin = clock();
@@ -59,8 +59,8 @@ int main(int argc, char **argv) {
             genxs = nodos_gen / secs;
 
             int h0 = gap_h(&state,NUMVARS);
-            int costo = static_cast<int>(get<1>(p));
-            cout << get<0>(p)<< ", "<< costo << ", " << h0 << ", "<< nodos_gen << ", " << secs << ", "<< genxs <<"\n" << flush;
+            //int costo = static_cast<int>(get<1>(p));
+            cout << get<0>(p)<< ", " << h0 << ", "<< nodos_gen << ", " << secs << ", "<< genxs <<"\n" << flush;
  			return 0;
     	}	
     }
@@ -68,7 +68,7 @@ int main(int argc, char **argv) {
 }
 //Iterative deepening depth-first search
 // <costo, generados, goal?>
-tuple<unsigned,unsigned,bool> bounded_id_dfs(state_t state, int  d, int  bound, int history){
+tuple<unsigned,unsigned,bool> bounded_id_dfs(state_t state, int  costoPadre, int  bound, int history){
 		int ruleid;
 	    ruleid_iterator_t posible_moves;
 	    state_t child;
@@ -77,27 +77,27 @@ tuple<unsigned,unsigned,bool> bounded_id_dfs(state_t state, int  d, int  bound, 
         int history_hijo;
         int f;
         //childCount = 0;
-
+        nodos_gen++;
         //Caso Base
-        f = d + gap_h(&state,NUMVARS);
+        f = costoPadre + gap_h(&state,NUMVARS);
         if (f > bound) return make_tuple(0, f, false);
         if (is_goal(&state)){
-            return (make_tuple(d, f, true));
+            return (make_tuple(costoPadre, costoPadre, true));
         }
 	    
 
         //expansion and recursion
-        int f2 = INT_MAX;
+        int t = INT_MAX;
         init_fwd_iter(&posible_moves, &state);
         while ( (ruleid = next_ruleid(&posible_moves)) >= 0 ) {
             if ( fwd_rule_valid_for_history(history,ruleid) == 0 ) continue;
 	        apply_fwd_rule(ruleid, &state, &child);
             history_hijo = next_fwd_history(history,ruleid);
 	        //++childCount;
-	        tuple<unsigned,unsigned,bool> px = bounded_id_dfs(child, d+1, bound, history_hijo);
+	        tuple<unsigned,unsigned,bool> px = bounded_id_dfs(child, costoPadre+1, bound, history_hijo);
             if ((get<2>(px))  == 1) return make_tuple(get<0>(px), f,true);  
-            f2=min(f2,static_cast<int>(get<1>(px)));
+            t=min(t,static_cast<int>(get<1>(px)));
 
         }
-        return make_tuple(0, f2,false);
+        return make_tuple(0, t,false);
 }
